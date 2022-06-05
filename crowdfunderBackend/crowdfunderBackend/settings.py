d26aec405a7be0a7716bcf15fbdeb9ce7e7cb559
Fraser Watt <fsrwatt@gmail.com>
2022-06-05 18:48:36 +0100

    New secret key not hardcoded(!) anymore

--- crowdfunderBackend/crowdfunderBackend/settings.py
+++ crowdfunderBackend/crowdfunderBackend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
+from dotenv import find_dotenv, load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
+load_dotenv(find_dotenv())
 
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-# UPDATE secret key
-SECRET_KEY = os.environ["SECRET_KEY"]  # Instead of your actual secret key
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
