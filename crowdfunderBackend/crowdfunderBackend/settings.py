c3dbd65b6b7d6051ede063a7ba88e0459e083874
Fraser Watt <fsrwatt@gmail.com>
2022-06-05 12:54:40 +0100

    adding CORS policy

--- crowdfunderBackend/crowdfunderBackend/settings.py
+++ crowdfunderBackend/crowdfunderBackend/settings.py
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "crowdfunderPledges",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -132,3 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:52361",
+    "http://localhost:3000",
+    "frontend-mentor-crowdfunder-app.vercel.app",
+]
